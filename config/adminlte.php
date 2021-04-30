bd50e46a6b9ad41ecb67cf32da9319e8a2a338e4
josue <josue.vitate@gmail.com>
2021-04-30 09:17:33 -0500

    favicon, logo

--- config/adminlte.php
+++ config/adminlte.php
@@ -30,8 +30,8 @@ return [
     |
     */
 
-    'use_ico_only' => false,
-    'use_full_favicon' => false,
+    'use_ico_only' => true,
+    'use_full_favicon' => true,
 
     /*
     |--------------------------------------------------------------------------
@@ -46,7 +46,8 @@ return [
     */
 
     'logo' => '<b>Inyawa</b>LTE',
-    'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    //'logo_img' => 'vendor/adminlte/dist/img/AdminLTELogo.png',
+    'logo_img' => 'img/logo_inyawa.jpg',
     'logo_img_class' => 'brand-image img-circle elevation-3',
     'logo_img_xl' => null,
     'logo_img_xl_class' => 'brand-image-xs',
