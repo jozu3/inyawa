617039261c492e642e12f31a4453f7ec7b0e6ba9
jozu3 <josue.vitate@gmail.com>
2021-06-16 13:25:24 -0500

    prueba

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Inyawa') }}</title>
+        <title>{{ config('app.name', 'Inyawaa') }}</title>
         <link rel="shortcut icon" href="{{ config('app.url', 'https://www.inyawaperu.com') }}/favicons/favicon.ico">
 
         <!-- Fonts -->
