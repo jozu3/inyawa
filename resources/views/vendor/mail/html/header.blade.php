ba7f1078346a8d023646e8a296eddaa3349ca37a
josue <josue.vitate@gmail.com>
2021-07-20 17:51:39 -0500

    logo en correo de resetpassword

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,7 +1,10 @@
 <tr>
 <td class="header">
+		
 <a href="{{ $url }}" style="display: inline-block;">
-<img src="{{ $url }}/img/logo_inyawa.jpg" class="logo" alt="Inyawa Logo">
+	<center style="border-radius: 50%;">
+		<img src="{{ $url }}/img/logo_inyawa.jpg" width="120px" class="logo" alt="Inyawa Logo">
+	</center>
 @if (trim($slot) === 'Inyawa')
 {{-- aquí iba el logo de laravel --}}
 @else
