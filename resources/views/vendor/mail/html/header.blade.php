ce10fb182841fee6bdb75c2a403ec0fff01fdf23
josue <josue.vitate@gmail.com>
2021-07-20 17:56:02 -0500

    stilos de logo en correo reset pas

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,8 +2,8 @@
 <td class="header">
 		
 <a href="{{ $url }}" style="display: inline-block;">
-	<center style="border-radius: 50%;">
-		<img src="{{ $url }}/img/logo_inyawa.jpg" width="120px" class="logo" alt="Inyawa Logo">
+	<center style="">
+		<img src="{{ $url }}/img/logo_inyawa.jpg" style="border-radius: 50% !important; height: auto!important; width:150px!important" class="logo" alt="Inyawa Logo">
 	</center>
 @if (trim($slot) === 'Inyawa')
 {{-- aquí iba el logo de laravel --}}
