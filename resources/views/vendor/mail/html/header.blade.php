85945dbc161e79d0667048d7c36a94b5fc0b47f5
josue <josue.vitate@gmail.com>
2021-07-20 18:01:25 -0500

    estilos de logo en mail resetpassw

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 		
 <a href="{{ $url }}" style="display: inline-block;">
 	<center style="">
-		<img src="{{ $url }}/img/logo_inyawa.jpg" style="border-radius: 50% !important; height: auto!important; width:150px!important" class="logo" alt="Inyawa Logo">
+		<img src="{{ $url }}/img/logo_inyawa.jpg" style="border-radius: 50% !important; max-height: 150px!important; width:150px!important; " class="logo" alt="Inyawa Logo">
 	</center>
 @if (trim($slot) === 'Inyawa')
 {{-- aquí iba el logo de laravel --}}
